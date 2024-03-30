986f8081a8488e57d2a4ac45bfdf71322df3d545
Gaillard Matthys <matthysgaillard@gmail.com>
2024-03-30 15:37:14 +0100

    Update base de données Le script d'initialisation de la BD fonctionne Le script et le schéma ont été adapté pour correspondre aux nouvelles contraintes des délivrables par période. De plus, le format json a été ajouté pour que l'on puisse ajouter plusieurs rôles pour une même personne

--- BD/docker-compose-postgres/init.sql
+++ BD/docker-compose-postgres/init.sql
@@ -10,26 +10,44 @@ INSERT INTO test (name, archived)
   VALUES ('test row 1', true),
   ('test row 2', false);
 */
+-- function for checking
+    CREATE OR REPLACE FUNCTION check_role_content(json_content JSON) RETURNS BOOLEAN AS $$
+DECLARE
+  role_value jsonb;
+BEGIN
+  role_value := json_content -> 'role';
+  RETURN role_value ? 'superviseur' OR role_value ? 'professeur' OR role_value ? 'etudiant' OR role_value ? 'admin';
+END;
+$$ LANGUAGE plpgsql;
 CREATE TABLE Personne(
   idPersonne INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   nom TEXT NOT NULL,
   prenom TEXT NOT NULL,
   mail TEXT NOT NULL UNIQUE CHECK (mail ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$'),
-  password TEXT NOT NULL CHECK (length(motDePasse) >= 8),
-  role TEXT NOT NULL CHECK(rolePersonne in ('etudiant', 'prof', 'admin','superviseur')),
+  password TEXT NOT NULL CHECK (length(password) >= 8),
+  role JSON NOT NULL CHECK (jsonb_typeof(role::jsonb -> 'role') = 'array' AND jsonb_exists(role::jsonb, 'role') and check_role_content(role)),
   last_login TEXT DEFAULT NULL,
   is_superuser BOOLEAN DEFAULT FALSE,
   is_staff BOOLEAN DEFAULT True,
-  is_active BOOLEAN DEFAULT True,
+  is_active BOOLEAN DEFAULT True
+);
+CREATE TABLE Delivrable(
+  idDelivrable INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  fichier TEXT,
+  typeFichier TEXT check(typeFichier IN ('pdf', 'docx', 'pptx', 'xlsx', 'zip', 'rar', '7z', 'tar', 'gz', 'bz2', 'xz', 'tgz', 'tbz2', 'txz', 'pdf', 'docx', 'pptx', 'xlsx', 'zip', 'rar', '7z', 'tar', 'gz', 'bz2', 'xz', 'tgz', 'tbz2', 'txz'))
 );
 CREATE TABLE Periode(
   idPeriode INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  annee int NOT NULL,
-  delaiPremierePartie DATE NOT NULL,
-  delaiDeuxiemePartie DATE NOT NULL,
-  delaiTroisiemePartie DATE NOT NULL,
-  delaiFinal DATE NOT NULL
-
+  annee int NOT NULL
+  );
+CREATE TABLE Etape(
+  idEtape INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  delai DATE NOT NULL,
+  description TEXT NOT NULL,
+  idPeriode INT NOT NULL,
+  idDelivrable INT,
+  FOREIGN KEY (idPeriode) REFERENCES Periode(idPeriode),
+  FOREIGN KEY (idDelivrable) REFERENCES Delivrable(idDelivrable)
 );
 CREATE TABLE Professeur(
   idProf INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -44,6 +62,7 @@ CREATE TABLE Sujet(
   titre TEXT NOT NULL,
   descriptif TEXT NOT NULL DEFAULT 'NULL',
   destination TEXT NOT NULL DEFAULT 'NULL',
+  estPris BOOLEAN NOT NULL DEFAULT FALSE,
   fichier TEXT, --  localisation du fichier de la proposition de sujet
   idPeriode INT NOT NULL DEFAULT 1,
   idProf INT NOT NULL DEFAULT 1,
@@ -57,17 +76,17 @@ CREATE TABLE UE(
   idProf INT NOT NULL,
   FOREIGN KEY (idProf) REFERENCES Professeur(idProf)
 );
-CREATE TABLE Cours( -- re
+CREATE TABLE Cours(
   idCours INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   idue TEXT NOT NULL,
   nom TEXT NOT NULL,
   FOREIGN KEY (idUE) REFERENCES UE(idUE)
 );
-CREATE TABLE ETUDIANT(
+CREATE TABLE Etudiant(
   idEtudiant INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   bloc INT NOT NULL check(bloc >= 1 and bloc <= 5),
   idPersonne INT NOT NULL,
-  idSujet INT NOT NULL,
+  idSujet INT UNIQUE,
   FOREIGN KEY (idPersonne) REFERENCES Personne(idPersonne),
   FOREIGN KEY (idSujet) REFERENCES Sujet(idSujet)
 );
@@ -82,38 +101,48 @@ CREATE TABLE Inscription(
 
 
 -- add insertion data
-INSERT INTO Personne (nom, prenom, mail,motDePasse, rolePersonne)
-  VALUES ('Doe', 'John', 'john.doe@gmail.com','pbkdf2_sha256$720000$tjC57NAqNFX9F7XCKvDqet$ymUne1VQexTF3EB/sqF+eqJSC8ZC4F9wgrSUblI9iPw=', 'etudiant'),
-        ('Doe', 'Jane', 'jane.doe@gmail.com','pbkdf2_sha256$720000$tjC57NAqNFX9F7XCKvDqet$ymUne1VQexTF3EB/sqF+eqJSC8ZC4F9wgrSUblI9iPw=','etudiant'),
-        ('Doe', 'Rick','ricke.doe@gmail.com','pbkdf2_sha256$720000$tjC57NAqNFX9F7XCKvDqet$ymUne1VQexTF3EB/sqF+eqJSC8ZC4F9wgrSUblI9iPw=','etudiant'),
-        ('Doe', 'Rudolf','rudolf.doe@gmail.com','pbkdf2_sha256$720000$tjC57NAqNFX9F7XCKvDqet$ymUne1VQexTF3EB/sqF+eqJSC8ZC4F9wgrSUblI9iPw=','etudiant'),
-        ('Doe', 'Jack', 'jack.doe@gmail.com','pbkdf2_sha256$720000$tjC57NAqNFX9F7XCKvDqet$ymUne1VQexTF3EB/sqF+eqJSC8ZC4F9wgrSUblI9iPw=','prof'),
-        ('Doe', 'Jill', 'jill.doe@gmail.com','pbkdf2_sha256$720000$tjC57NAqNFX9F7XCKvDqet$ymUne1VQexTF3EB/sqF+eqJSC8ZC4F9wgrSUblI9iPw=','prof'),
-        ('Doe', 'James', 'james.doe@gmail.com','pbkdf2_sha256$720000$tjC57NAqNFX9F7XCKvDqet$ymUne1VQexTF3EB/sqF+eqJSC8ZC4F9wgrSUblI9iPw=','admin'),
-        ('Doe', 'Jenny', 'jenny.doe@gmail.com','pbkdf2_sha256$720000$tjC57NAqNFX9F7XCKvDqet$ymUne1VQexTF3EB/sqF+eqJSC8ZC4F9wgrSUblI9iPw=','superviseur');
-INSERT INTO Periode (annee, delaiPremierePartie, delaiDeuxiemePartie, delaiTroisiemePartie, delaiFinal)
-  VALUES (EXTRACT(YEAR FROM TIMESTAMP '2024-01-01'), '2024-02-02', '2024-03-12', '2024-04-28', '2022-05-27'),
-        (EXTRACT(YEAR FROM TIMESTAMP '2024-01-01'), '2024-02-02', '2024-03-12', '2024-04-28', '2022-05-27');
+INSERT INTO Personne (nom, prenom, mail,password, role)
+  VALUES ('Doe', 'John', 'john.doe@gmail.com', 'pbkdf2_sha256$720000$tjC57NAqNFX9F7XCKvDqet$ymUne1VQexTF3EB/sqF+eqJSC8ZC4F9wgrSUblI9iPw=',
+          '{"role" : ["etudiant"]}'),
+        ('Doe', 'Jane', 'jane.doe@gmail.com','pbkdf2_sha256$720000$tjC57NAqNFX9F7XCKvDqet$ymUne1VQexTF3EB/sqF+eqJSC8ZC4F9wgrSUblI9iPw=','{"role" : ["etudiant"]}'),
+        ('Doe', 'Rick','ricke.doe@gmail.com','pbkdf2_sha256$720000$tjC57NAqNFX9F7XCKvDqet$ymUne1VQexTF3EB/sqF+eqJSC8ZC4F9wgrSUblI9iPw=','{"role" : ["etudiant"]}'),
+        ('Doe', 'Rudolf','rudolf.doe@gmail.com','pbkdf2_sha256$720000$tjC57NAqNFX9F7XCKvDqet$ymUne1VQexTF3EB/sqF+eqJSC8ZC4F9wgrSUblI9iPw=','{"role" : ["etudiant"]}'),
+        ('Doe', 'Jack', 'jack.doe@gmail.com','pbkdf2_sha256$720000$tjC57NAqNFX9F7XCKvDqet$ymUne1VQexTF3EB/sqF+eqJSC8ZC4F9wgrSUblI9iPw=','{"role" : ["professeur","superviseur"]}'),
+        ('Doe', 'Jill', 'jill.doe@gmail.com','pbkdf2_sha256$720000$tjC57NAqNFX9F7XCKvDqet$ymUne1VQexTF3EB/sqF+eqJSC8ZC4F9wgrSUblI9iPw=', '{"role" : ["professeur"]}'),
+        ('Doe', 'James', 'james.doe@gmail.com','pbkdf2_sha256$720000$tjC57NAqNFX9F7XCKvDqet$ymUne1VQexTF3EB/sqF+eqJSC8ZC4F9wgrSUblI9iPw=', '{"role" : ["admin"]}'),
+        ('Doe', 'Jenny', 'jenny.doe@gmail.com','pbkdf2_sha256$720000$tjC57NAqNFX9F7XCKvDqet$ymUne1VQexTF3EB/sqF+eqJSC8ZC4F9wgrSUblI9iPw=','{"role": ["superviseur"]}');
+INSERT INTO Periode (annee)
+  VALUES (EXTRACT(YEAR FROM TIMESTAMP '2024-01-01')),
+        (EXTRACT(YEAR FROM TIMESTAMP '2024-01-01'));
+INSERT INTO Delivrable (fichier, typeFichier)
+  VALUES ('/home/etudiant/devoir1.pdf', 'pdf'),
+        ('/home/etudiant/devoir2', 'docx');
+INSERT INTO Etape (delai, description, idPeriode, idDelivrable)
+  VALUES ('2024-01-01', 'rendre le devoir 1', 1, 1),
+        ('2024-02-01', 'rendre le devoir 2', 2, 2);
 
 INSERT INTO Professeur (specialite, idPersonne, idPeriode)
   VALUES ('IA', 3, 1),
         ('ML', 4, 2);
-INSERT INTO Sujet (titre, descriptif, fichier, idPeriode, idProf)
-  VALUES ('La reproduction des insectes', 'Les insectes sont des animaux ovipares', NULL, 1, 1),
-        ('L IA', 'L intelligence artificelle est un système informatique capable d apprendre par lui-même', NULL, 2, 2);
+INSERT INTO Sujet (titre, descriptif, fichier, idPeriode, idProf,estPris)
+  VALUES ('La reproduction des insectes', 'Les insectes sont des animaux ovipares', NULL, 1, 1,TRUE),
+        ('L IA', 'L intelligence artificelle est un système informatique capable d apprendre par lui-même', NULL, 2, 2,TRUE);
 INSERT INTO UE (idue,nom, idProf)
   VALUES ('INFOB331','Introduction à la démarche scientifique', 1),
         ('INFOMA451','Mémoire', 2);
 INSERT INTO Cours (idUE, nom)
   VALUES ('INFOB331', 'Introduction à la démarche scientifique'),
         ('INFOMA451', 'Mémoire');
-INSERT INTO ETUDIANT (bloc, idPersonne, idSujet)
+INSERT INTO Etudiant (bloc, idPersonne, idSujet)
   VALUES (1, 1, 1),
-        (2, 2, 2),
-        (3, 3, 1),
-        (4, 4, 2);
+        (2, 2, 2);
+INSERT INTO Etudiant(bloc, idPersonne)
+      VALUES  (3, 3),
+        (4, 4);
 INSERT INTO Inscription (idEtudiant, idCours)
   VALUES (1, 1),
         (2, 2),
         (3, 1),
         (4, 2);
+
+
